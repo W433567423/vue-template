a691d780a496c6951ac037b978c26485ce764194
tutu <t433567423@163.com>
2023-03-31 14:46:09 +0800

    feat: vuex

---
+++ src/store/index.ts
@@ -0,0 +1,18 @@
+import Vuex from 'vuex'
+export default new Vuex.Store({
+  state: {
+    // 存放数据
+  },
+  mutations: {
+    // 修改state里面的数据
+  },
+  getters: {
+    // 计算属性
+  },
+  actions: {
+    // vuex中发起异步请求
+  },
+  modules: {
+    // 模块
+  }
+})
