c85da83b17a745d6fc5ce00fd139132b0ee4c267
tutu <t433567423@163.com>
2023-03-31 15:32:40 +0800

    feat: vue-router

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import store from './store'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).use(store).mount('#app')
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.mount('#app')
---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="index">这是index页面</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="less"></style>
